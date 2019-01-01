487930592a113c4ddc8e0d518a733375ed3b94db
paul <paul.souille85@gmail.com>
2019-01-01 15:45:07 +0100

    comment routes

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -11,41 +11,65 @@ export class ApiProvider {
     private readonly http: HttpClient
       ) {
   }
-   
-  getEvent():Observable<any>{
-    var url = this.baseUrl+'/event/get';
-    return this.http.get(`${url}`);
-  }
-  login(login,password): Observable<any> {
-      var url = this.baseUrl+'/login?login='+login+'&password='+password;
-      return this.http.get(`${url}`);
-  }
-  addEvent(event,response,user_id):Observable<any>{
-    var url = this.baseUrl+'/event/add?event='+event+'&response='+response+'&user_id='+user_id;
-    return this.http.get(`${url}`);
-  }
-  deleteEvent(id,apiKey,user_id):Observable<any>{
-    var url = this.baseUrl+'/event/delete?event_id='+id+'&apiKey='+apiKey+'&user_id='+user_id;
-    return this.http.get(`${url}`);
-  }
-  askDelete(message_id):Observable<any>{
-    var url = this.baseUrl+'/event/askDelete?message_id='+message_id;
-    return this.http.get(`${url}`);
-  }
-  removeDelete(message_id):Observable<any>{
-    var url = this.baseUrl+'/event/removeDelete?message_id='+message_id;
-    return this.http.get(`${url}`);
-  }
-  enableUser(user_id):Observable<any>{
-    var url = this.baseUrl+'/users/enableUser?user_id='+user_id;
-    return this.http.get(`${url}`);
-  }
-  disableUser(user_id):Observable<any>{
-    var url = this.baseUrl+'/users/disableUser?user_id='+user_id;
-    return this.http.get(`${url}`);
-  }  
+
+  //GET
+  //{{api}}/users
   getUsers():Observable<any>{
     var url = this.baseUrl+'/users/';
     return this.http.get(`${url}`);
   }  
+
+  //PUT
+  //{{api}}/users/login
+  login(login,password): Observable<any> {
+      var url = this.baseUrl+'/login?login='+login+'&password='+password;
+      return this.http.get(`${url}`);
+  }
+  //PUT
+  //{{api}}/user/enable/id
+  enableUser(user_id):Observable<any>{
+    var url = this.baseUrl+'/users/enableUser?user_id='+user_id;
+    return this.http.get(`${url}`);
+  }
+  //PUT
+  //{{api}}/user/disable/id
+  disableUser(user_id):Observable<any>{
+    var url = this.baseUrl+'/users/disableUser?user_id='+user_id;
+    return this.http.get(`${url}`);
+  }  
+  //PUT
+  //{{api}}/messages
+  addEvent(event,response,user_id):Observable<any>{
+    var url = this.baseUrl+'/event/add?event='+event+'&response='+response+'&user_id='+user_id;
+    return this.http.get(`${url}`);
+  }
+  
+  //DELETE
+  //{{api}}/message/id
+  deleteEvent(id,apiKey,user_id):Observable<any>{
+    var url = this.baseUrl+'/event/delete?event_id='+id+'&apiKey='+apiKey+'&user_id='+user_id;
+    return this.http.get(`${url}`);
+  }
+
+  //PUT
+  //{{api}}/messages/askDelete/id
+  askDelete(message_id):Observable<any>{
+    var url = this.baseUrl+'/event/askDelete?message_id='+message_id;
+    return this.http.get(`${url}`);
+  }
+
+  //Rassembler les deux en une seule
+  //PUT
+  //{{api}}/messages/askDelete/id
+  removeDelete(message_id):Observable<any>{
+    var url = this.baseUrl+'/event/removeDelete?message_id='+message_id;
+    return this.http.get(`${url}`);
+  }
+
+  //GET 
+  //{{api}}/messages
+  getEvent():Observable<any>{
+    var url = this.baseUrl+'/event/get';
+    return this.http.get(`${url}`);
+  }
 } 
